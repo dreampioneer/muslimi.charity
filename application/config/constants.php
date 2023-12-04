79ecbcb9501c5254696ba8276499acb3b2438b22
dreampioneer <dreampioneer9999@gmail.com>
2023-12-04 20:38:51 +0900

    working on home content

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,16 +1,17 @@
 <?php
     return [
-        'home_content' => "🤝 Our charity partners are delivering aid to our brothers and sisters in Gaza.<br>
-            ⚠ The overall death toll in Palestine is 11,000+ (40% are children), and over 28,000+ are injured. 2.3 million people are at risk.<br>
-            💡 Muslimi is working to ensure your aid is delivered in Gaza. Your donation is an Amana, which will reach our partners on the ground. Our charity partners have emergency aid stockpiles and are getting resources from within Gaza; though these resources are being reduced daily, they are being replenished as the Egypt-Rafah border crossing is slowly letting aid in inshaAllah.<br>
-            The Rafah border crossing between Egypt and Gaza has opened to let needed aid flow to Palestinians running short of food, medicine, and water in Gaza. Meanwhile, aid deliveries have come as the Israeli military continued bombing Gaza and Rafah.<br>
-            Your donation right now can be the lifeline for many in Gaza. Let's unite in this hour of dire need and show that the Ummah stands united with the innocent civilians in Gaza.<br>
-            🆘 Supply a family with a month's supply of Hot Meals - $56.00<br>
-            🆘 Supply 2 families with a month's supply of Hot Meals - $112.00<br>
-            🆘 Supply 5 families with a month's supply of Hot Meals - $280.00<br>
-            🆘 Supply 10 families with a month's supply of Hot Meals - $560.00<br>
-            🆘 Supply 20 families with a month's supply of Hot Meals - $1,120.00<br>
-            🆘 Emergency Medical Supplies to Hospitals - $200<br>
-            🆘 Emergency Shelter - $500<br>
-            🆘 Emergency Aid Combo (Meals, Water, Aid, Shelter) - $1,000"
+        'home_content' => "We are a group of Muslim volunteers based in Egypt.
+            Our volunteers are delivering aid to the Gaza bombardment victims via Rafah border crossing between Egypt and Gaza.
+            ⚠ The overall death toll in Gaza is 15,000+ (40% are children), with over 7,000 missing and over 40,000 injured. 2.3 million people are at risk.
+            Muslimi is working to ensure your aid is delivered in Gaza. Your donation is an Amana, which will reach our volunteers on the ground in Egypt and Gaza.
+            The Rafah border crossing between Egypt and Gaza has opened to let needed aid flow to Palestinians running short of food, medicine, and water in Gaza.
+            Your donation right now can be the lifeline for many in Gaza. Let's unite in this hour of dire need and show that the Ummah stands united with the innocent civilians in Gaza.
+            Supply a family with a month's supply of Hot Meals - €56.00
+            Supply 2 families with a month's supply of Hot Meals - €112.00
+            Supply 5 families with a month's supply of Hot Meals - €280.00
+            Supply 10 families with a month's supply of Hot Meals - €560.00
+            Supply 20 families with a month's supply of Hot Meals - €1,120.00
+            Emergency Medical Supplies to Hospitals - €200
+            Emergency Shelter - €500
+            Emergency Aid Combo (Meals, Water, Aid, Shelter) - €1,000"
     ];
