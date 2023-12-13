e2534fa4e4f9eb760c1f7cab64b2b99749964f9a
dreampioneer <dreampioneer9999@gmail.com>
2023-12-14 08:07:52 +0900

    working on update payment controller

--- application/app/Http/Controllers/PaymentController.php
+++ application/app/Http/Controllers/PaymentController.php
@@ -13,43 +13,16 @@ use Illuminate\Http\Response;
 
 class PaymentController extends Controller
 {
+    private $stripe;
+
+    public function __construct(){
+        $this->stripe = new StripeClient(env('STRIPE_SECRET'));
+    }
+
     public function index()
     {
         return view('stripe.index');
     }
-
-    // Test
-
-    function confirmPayment(Request $request){
-        $stripe = new StripeClient(env('STRIPE_SECRET'));
-        $stripe->paymentIntents->confirm($request->paymentIntentId,
-        [
-            'return_url' => route('stripe.index'),
-        ]);
-        $paymentIntent = $stripe->paymentIntents->retrieve($request->paymentIntentId);
-        return json_encode($paymentIntent);
-    }
-
-    function createPaymentIntents(Request $request){
-        $stripe = new StripeClient(env('STRIPE_SECRET'));
-        $paymentIntent = $stripe->paymentIntents->create([
-            'amount' => 1999,  // Replace with your desired amount in cents
-            'currency' => 'eur',
-            'confirmation_method' => 'manual',
-            'confirm' => true,
-            'payment_method_data' => [
-                'type' => 'card',
-                'card' => [
-                    'token' => $request->card_token,  // Card token from your frontend
-                ],
-            ],
-            'return_url' => route('stripe.threeDS'),
-        ]);
-
-        return json_encode($paymentIntent);
-    }
-
-    //
 
     function createPaymentIntent(Request $request){
         $validate = Validator::make($request->all(), [
@@ -70,9 +43,8 @@ class PaymentController extends Controller
         }
 
         $email = $request->email;
-        $stripe = new StripeClient(env('STRIPE_SECRET'));
 
-        $customers = $stripe->customers->all([
+        $customers = $this->stripe->customers->all([
             "limit" => 10,
             "email" => $email,
         ]);
@@ -85,9 +57,9 @@ class PaymentController extends Controller
         }
 
         if (isset($customerId)) {
-            $customer = $stripe->customers->retrieve($customerId);
+            $customer = $this->stripe->customers->retrieve($customerId);
         } else {
-            $customer = $stripe->customers->create(
+            $customer = $this->stripe->customers->create(
                 [
                     "email" => $request->email,
                     "name" => $request->first_name . " " . $request->last_name,
@@ -104,7 +76,7 @@ class PaymentController extends Controller
         }
 
         if ($request->is_monthly == 'true') {
-            $products = $stripe->products->all();
+            $products = $this->stripe->products->all();
             $priceIds = [];
             foreach($products as $product ){
                 $priceIds[$product->name] = $product->default_price;
@@ -118,19 +90,19 @@ class PaymentController extends Controller
                 ]);
             }
 
-            $paymentMethod = $stripe->paymentMethods->create([
+            $paymentMethod = $this->stripe->paymentMethods->create([
                 'type' => 'card',
                 'card' => [
                     'token' => $request->stripeToken,
                 ],
             ]);
 
-            $paymentMethod = $stripe->paymentMethods->attach(
+            $paymentMethod = $this->stripe->paymentMethods->attach(
                 $paymentMethod->id,
                 ['customer' => $customer->id]
             );
 
-            $subscription = $stripe->subscriptions->create([
+            $subscription = $this->stripe->subscriptions->create([
                 'customer' => $customer->id,
                 'items' => $items,
                 'off_session' => true,
@@ -142,7 +114,7 @@ class PaymentController extends Controller
                 'expand' => ['latest_invoice.payment_intent'],
                 "description" => $request->dedicate_this_donation ? "Double-Time Donate - " . $request->dedicate_this_donation : "Double-Time Donate",
             ]);
-            $paymentIntent = $stripe->paymentIntents->retrieve($subscription->latest_invoice->payment_intent->id);
+            $paymentIntent = $this->stripe->paymentIntents->retrieve($subscription->latest_invoice->payment_intent->id);
             if($paymentIntent->status === 'requires_action'){
                 $paymentIntent->confirm([
                     'return_url' => route('stripe.threeDS'),
@@ -151,7 +123,7 @@ class PaymentController extends Controller
             return json_encode($paymentIntent);
         } else {
 
-            $paymentIntent = $stripe->paymentIntents->create([
+            $paymentIntent = $this->stripe->paymentIntents->create([
                 'amount' => $total * 100,  // Replace with your desired amount in cents
                 'currency' => 'eur',
                 'confirmation_method' => 'manual',
@@ -172,9 +144,8 @@ class PaymentController extends Controller
     }
 
     function confirmPaymentIntent(Request $request){
-        $stripe = new StripeClient(env('STRIPE_SECRET'));
-        $stripe->paymentIntents->confirm($request->paymentIntentId);
-        $paymentIntent = $stripe->paymentIntents->retrieve($request->paymentIntentId);
+        $this->stripe->paymentIntents->confirm($request->paymentIntentId);
+        $paymentIntent = $this->stripe->paymentIntents->retrieve($request->paymentIntentId);
         return json_encode($paymentIntent);
     }
 
@@ -230,130 +201,51 @@ class PaymentController extends Controller
         return response()->json($data);
     }
 
-    public function store(Request $request)
-    {
-        $validate = Validator::make($request->all(), [
-            'stripeToken' => ['required'],
-            'card_number' => ['required'],
-            'cvc' => ['required', 'numeric'],
-            'expirey_date' => ['required'],
-            'first_name' => ['required', 'max:255'],
-            'last_name' => ['required', 'max:255'],
-            'email' => ['required', 'email'],
-            'donates' => ['required', 'array', 'min:1'],
-            'dedicate_this_donation' => ['max:40000'],
-            // 'is_zakat' => ['boolean'],
-        ]);
-
-        if ($validate->fails()) {
-            return back()->withErrors($validate->errors())->withInput();
-        }
-
-        try {
-            $email = $request->email;
-            $stripe = new StripeClient(env('STRIPE_SECRET'));
-
-            $customers = $stripe->customers->all([
-                "limit" => 100,
-                "email" => $email,
-            ]);
-
-            foreach ($customers->autoPagingIterator() as $customer) {
-                if ($customer->name == $request->first_name . " " . $request->last_name) {
-                    $customerId = $customer->id;
-                    break;
-                }
-            }
-
-            if (isset($customerId)) {
-                $customer = $stripe->customers->retrieve($customerId);
-            } else {
-                $customer = $stripe->customers->create(
-                    [
-                        "email" => $request->email,
-                        "name" => $request->first_name . " " . $request->last_name,
-                        "source" => $request->stripeToken
-                    ]
-                );
-            }
-
-            $total = 0;
-
-            foreach ($request->donates as $donate) {
-                $subtotal = intval($donate['donate_count']) * floatval($donate['donate_amount']);
-                $total += $subtotal;
-            }
-
-            if ($request->is_monthly == 'true') {
-                $items = [];
-                foreach ($request->donates as $donate) {
-                    array_push($items, [
-                        'price' => $donate['donate_price_id'],
-                        'quantity' => intval($donate['donate_count'])
-                    ]);
-                }
-
-                $result = $stripe->subscriptions->create([
-                    'customer' => $customer->id,
-                    'items' => $items,
-                    'payment_behavior' => 'default_incomplete',
-                    'payment_settings' => ['save_default_payment_method' => 'on_subscription'],
-                    'expand' => ['latest_invoice.payment_intent'],
-                    "description" => $request->dedicate_this_donation ? "Double-Time Donate - " . $request->dedicate_this_donation : "Double-Time Donate",
-                ]);
-
-                $invoice = $stripe->invoices->pay($result->latest_invoice->id, [
-                    "source" => $customer->default_source
-                ]);
-            } else {
-
-                $result = $stripe->charges->create([
-                    "amount" => $total * 100,
-                    "currency" => "usd",
-                    "customer" => $customer->id,
-                    "description" => $request->dedicate_this_donation ? "One-Time Donate - " . $request->dedicate_this_donation : "One-Time Donate",
-                ]);
-            }
-
-            $donateHistory = DonateHistory::create([
-                'first_name' => $request->first_name,
-                'last_name' => $request->last_name,
-                'email' => $request->email,
-                'customer_id' => $customer->id,
-                'dedicate_this_donation' => $request->dedicate_this_donation,
-                'is_zakat' => $request->is_zakat == 'true' ? 1 : 0,
-                'is_monthly' => $request->is_monthly == 'true' ? 1 : 0,
-                'price' => $total,
-                'transaction_id' => $result->id,
-            ]);
-
-            foreach ($request->donates as $donate) {
-                DonateDetail::create([
-                    'donate_history_id' => $donateHistory->id,
-                    'donate_name' => $donate["donate_name"],
-                    'donate_amount' => $donate["donate_amount"],
-                    'donate_count' => $donate["donate_count"],
-                ]);
-            }
-
-            $data = [
-                'status' => 'success',
-                'msg' => '',
-                'return_url' => route('home.thanks', $donateHistory->id)
-            ];
-
-            $request->session()->flash('success', 'You have donated successfully!');
-            return response()->json($data);
-        } catch (CardException $th) {
-            return response()->json(['status' => 'error', 'msg' => $th->getMessage()]);
-        }
-    }
-
     public function threeDS(){
         return view('stripe.3ds');
     }
 
-    public function webHook(){
+    public function webHook(Request $request){
+        $payload = $request->getContent();
+        $sigHeader = $request->header('Stripe-Signature');
+        $endpointSecret = env('STRIPE_WEBHOOK');
+        try {
+            $event = \Stripe\Webhook::constructEvent(
+                $payload, $sigHeader, $endpointSecret
+            );
+        } catch (\UnexpectedValueException $e) {
+            // Invalid payload
+            return response()->json(['error' => 'Invalid payload'], 400);
+        } catch (\Stripe\Exception\SignatureVerificationException $e) {
+            // Invalid signature
+            return response()->json(['error' => 'Invalid signature'], 400);
+        }
 
+        $this->handleStripeEvent($event);
+
+        return response()->json(['success' => true]);
+    }
+
+    public function handleStripeEvent($event){
+        switch ($event->type) {
+            case 'invoice.paid':
+                $invoice = $event->data->object;
+                // $stripe->invoices->sendInvoice($invoice->id, []);
+                // Log::info('Invoice {id} is paid. \r\n Detail: {data} \r\n', ['id' => $invoice->id, 'data' => json_encode($invoice)]);
+                break;
+            case 'invoice.payment_action_required':
+                $invoice = $event->data->object;
+                // $stripe->invoices->update($invoice->id, [
+                //     'collection_method' => 'send_invoice'
+                // ]);
+                // $stripe->invoices->sendInvoice($invoice->id, []);
+                // Log::info('Invoice {id} required payment. \r\n Detail: {data}', ['id' => $invoice->id, 'data' => json_encode($invoice)]);
+                break;
+            case 'invoice.sent':
+                $invoice = $event->data->object;
+                // Log::info('Invoice {id} is sent.', ['id' => $invoice->id]);
+            default:
+                echo 'Received unknown event type ' . $event->type;
+        }
     }
 }
--- application/routes/web.php
+++ application/routes/web.php
@@ -31,17 +31,11 @@ Route::name('stripe.')
     ->controller(PaymentController::class)
     ->prefix('stripe')
     ->group(function () {
-        // Route::get('/payment', 'index')->name('index');
-        // Route::post('/payment', 'store')->name('store');
         Route::post('/create-paymentIntent', 'createPaymentIntent')->name('createPaymentIntent');
         Route::post('/confirm-paymentIntent', 'confirmPaymentIntent')->name('confirmPaymentIntent');
         Route::post('/create-donate-history', 'createDonateHistory')->name('createDonateHistory');
         Route::post('/webhook', 'webHook')->name('webHook');
         Route::get('/3ds', 'threeDS')->name('threeDS');
-
-        // For Test
-        // Route::post('/confirm-payment', 'confirmPayment')->name('confirmPayment');
-        // Route::post('/create-payment', 'createPaymentIntents')->name('createPaymentIntents');
     });
 
 Route::name('faq.')
